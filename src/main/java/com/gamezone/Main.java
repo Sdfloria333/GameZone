3170e9bd1df7cde9b0e2c20dbb35e019dc7deb78
florian <sdflorian@unicesar.edu.co>
2026-09-17 09:28:48 -0500

    feat:implement section for accessories ui

--- src/main/java/com/gamezone/Main.java
+++ src/main/java/com/gamezone/Main.java
@@ -3,6 +3,7 @@ package com.gamezone;
 import com.gamezone.service.PersonService;
 import com.gamezone.service.ProductService;
 import com.gamezone.service.SaleService;
+import com.gamezone.service.AccessoryService;
 import com.gamezone.ui.ConsoleUI;
 
 public class Main {
@@ -11,8 +12,9 @@ public class Main {
         ProductService productService = new ProductService();
         PersonService personService = new PersonService();
         SaleService saleService = new SaleService(productService, personService);
+        AccessoryService accessoryService = new AccessoryService();
 
-        ConsoleUI ui = new ConsoleUI(productService, personService, saleService);
+        ConsoleUI ui = new ConsoleUI(productService, personService, saleService, accessoryService);
         ui.start();
     }
 }
